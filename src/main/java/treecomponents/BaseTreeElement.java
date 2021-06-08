5fc2c2cdeaee7d47962dbf50f354a88ec61a59a9
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-06-09 00:37:12 +0300

    Creating a Base Tree Element

---
+++ src/main/java/treecomponents/BaseTreeElement.java
@@ -0,0 +1,39 @@
+package treecomponents;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BaseTreeElement {
+
+    //Auto increment
+    private static int count = 0;
+
+    private int serial_id;
+    private List<BaseTreeElement> parents = null;
+    private List<BaseTreeElement> children = null;
+
+    public BaseTreeElement(){
+        serial_id = count++;
+    }
+
+    public int getSerialId() {
+        return serial_id;
+    }
+
+    public void addChild(BaseTreeElement child) {
+        if (child.parents == null){
+            child.parents = new ArrayList<>();
+        }
+        child.parents.add(this);
+
+        if (children == null){
+            children = new ArrayList<>();
+        }
+        children.add(child);
+    }
+
+    public List<BaseTreeElement> getChildren() {
+        return children;
+    }
+}
