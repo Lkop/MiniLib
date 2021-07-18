c10b74fce2685da8a51ff977da96af9cdff2bff0
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-07-18 22:55:42 +0300

    Updating main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         ClassCreator cc = new ClassCreator("Generated");
 
         StringBuilder my_method = new StringBuilder();
@@ -13,11 +15,20 @@ public class Main {
         cc.addMethod(my_method);
         cc.createClassFile();
 
+
+        ClassParser cp = new ClassParser("C:\\Users\\Loukas\\Desktop\\MiniLibWorkspace\\Client\\out\\artifacts\\Client_jar\\Client.jar");
+
         ClassInsider ci = new ClassInsider("C:\\Users\\Loukas\\Desktop\\MiniLibWorkspace\\Client\\out\\artifacts\\Client_jar\\Client.jar");
         ci.assignStartingMethod("Main", "main");
-        ci.listCalledMethods();
+        ci.listCalledMethods(cp.getClasses());
 
         MethodElement method_tree = ci.getRoot();
-        System.out.println("Done");
+        System.out.println("Visiting Done");
+
+        TreePrinterVisitor tree_printer = new TreePrinterVisitor("ast");
+        tree_printer.openWriteGraph();
+        ci.getRoot().accept(tree_printer);
+        tree_printer.closeWriteGraph();
+        System.out.println("Printing Done");
     }
 }
