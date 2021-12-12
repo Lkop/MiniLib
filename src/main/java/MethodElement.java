45b0d596cc9a1719b205fc215e7a790089f6afcb
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-12-12 02:29:16 +0200

    Changing MethodElement fields

--- src/main/java/MethodElement.java
+++ src/main/java/MethodElement.java
@@ -1,23 +1,57 @@
-import treecomponents.BaseTreeElement;
+import javassist.CtClass;
 import treecomponents.BaseVisitor;
 import treecomponents.VisitableBaseTreeElement;
+import utils.StringUtils;
 
 public class MethodElement extends VisitableBaseTreeElement {
 
+    private String class_longname;
     private String class_name;
     private String method_name;
+    private CtClass[] params;
+    private String method_signature;
+    private String unique_id;
+    private String class_path;
+    private Enum.Methods type;
 
-    public MethodElement(String class_name, String method_name) {
-        this.class_name = class_name;
+    public MethodElement(String class_longname, String method_name, CtClass[] params, String method_signature, Enum.Methods type) {
+        this.class_longname = class_longname;
+        this.class_name = StringUtils.longToShortClassname(class_longname);
         this.method_name = method_name;
+        this.params = params;
+        this.method_signature = method_signature;
+        this.unique_id = class_longname + "+" + method_name + "+" + method_signature;
+        this.class_path = StringUtils.classToPath(class_longname);
+        this.type = type;
     }
 
     public String getClassName() {
         return class_name;
     }
 
+
+    public String getClassLongName() {
+        return class_longname;
+    }
+
     public String getMethodName() {
         return method_name;
+    }
+
+    public CtClass[] getParams() {
+        return params;
+    }
+
+    public String getUniqueId() {
+        return unique_id;
+    }
+
+    public String getClassPath() {
+        return class_path;
+    }
+
+    public Enum.Methods getType() {
+        return type;
     }
 
     public MethodElement getFirstParent() {
