1445d97bb7cc3dde6ed4705e82b4c5bfe5bb53ec
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-12-12 02:28:23 +0200

    Adding Enum

---
+++ src/main/java/Enum.java
@@ -0,0 +1,17 @@
+public class Enum {
+    public enum Fields {
+
+    }
+
+    public enum Methods {
+        CONSTRUCTOR,
+        METHOD
+    }
+
+    public enum ExprCall {
+        NEWEXPR_CALL,
+        NEWARRAY_CALL,
+        METHOD_CALL,
+        CONSTRUCTOR_CALL
+    }
+}
