bc5fe00e9f2cf6bb94411335f73e83e67162065f
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-12-12 02:23:59 +0200

    Adding ClassCreator

---
+++ src/main/java/ClassCreator.java
@@ -0,0 +1,68 @@
+import javassist.*;
+
+public class ClassCreator {
+
+    private ClassPool old_classpool, new_classpool;
+    private CtClass new_class;
+
+    public ClassCreator(){
+        this.old_classpool = ClassPool.getDefault();
+        this.new_classpool = new ClassPool();
+        this.new_classpool.appendClassPath(new LoaderClassPath(this.old_classpool.getClassLoader()));
+    }
+
+    public boolean copyExistingMethod(String class_longname, String method_name, CtClass[] params) {
+        initializeCtClass(class_longname);
+        try {
+            CtClass old_class = old_classpool.getCtClass(class_longname);
+            CtMethod old_method = old_class.getDeclaredMethod(method_name, params);
+            new_class.addMethod(CtNewMethod.copy(old_method, new_class, null));
+
+//            ////
+//            CtClass selected_class2 = class_pool.getCtClass("com.google.gson.Gson");
+//            CtConstructor[] ct_con = selected_class2.getDeclaredConstructors();
+//            new_class.addConstructor(CtNewConstructor.copy(ct_con[1], new_class, null));
+//            ////
+        }catch(CannotCompileException | NotFoundException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    public ClassPool getNewClassPool() {
+        return new_classpool;
+    }
+
+    private CtClass initializeCtClass(String class_longname) {
+        try {
+            new_class = new_classpool.getCtClass(class_longname);
+        } catch (NotFoundException e) {
+            new_class = new_classpool.makeClass(class_longname);
+            copyExistingFields(class_longname);
+        }
+        return new_class;
+    }
+
+    public boolean copyExistingFields(String class_longname) {
+        try {
+            CtClass old_class = old_classpool.getCtClass(class_longname);
+            for(CtField ct_field : old_class.getDeclaredFields()) {
+                new_class.addField(new CtField(ct_field, new_class));
+            }
+        } catch (CannotCompileException | NotFoundException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+//    private void createClassFile(String class_name) {
+//        try {
+//            ClassFile cf = new_classpool.get(class_name).getClassFile();
+//            File file = new File("output/" + classname + ".class");
+//            cf.write(new DataOutputStream(new FileOutputStream(file)));
+//        }catch (NotFoundException | IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+}
