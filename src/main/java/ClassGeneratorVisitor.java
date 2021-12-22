8c2620ee95f3cc0ea423e8421da722b35aa5be12
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-12-22 02:49:51 +0200

    Adding all generating methods

--- src/main/java/ClassGeneratorVisitor.java
+++ src/main/java/ClassGeneratorVisitor.java
@@ -6,24 +6,25 @@ public class ClassGeneratorVisitor extends MethodElementVisitor<Integer> {
         pc = new PackageCreator();
     }
 
-//    @Override
-//    public Integer visitStartingMethodElement(MethodElement node) {
-//        super.visitMethodElement(node);
-//        return 0;
-//    }
+    @Override
+    public Integer visitStartingMethodElement(StartingMethodElement node) {
+        super.visitMethodElement(node);
+        return 0;
+    }
 
     @Override
     public Integer visitMethodElement(MethodElement node) {
         System.out.println("Copying: " + node.getClassLongName() + " -> " + node.getMethodName() + "()");
-
         pc.addToPackage(node);
-
         super.visitMethodElement(node);
-
         return 0;
     }
 
-//    public void createClassFile() {
-//        cc.createClassFile();
-//    }
+    @Override
+    public Integer visitConstructorElement(ConstructorElement node) {
+        System.out.println("Copying constructor: " + node.getClassLongName() + " -> " + node.getMethodName() + "()");
+        pc.addToPackage(node);
+        super.visitConstructorElement(node);
+        return 0;
+    }
 }
