600a0bd28d9337aa3088e92dc9cf06b7952aa534
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-12-22 02:46:27 +0200

    Changing to .jpg format

--- src/main/java/utils/ImageUtils.java
+++ src/main/java/utils/ImageUtils.java
@@ -9,7 +9,7 @@ public final class ImageUtils {
     }
 
     public static void createGIF(String filename) throws IOException, InterruptedException {
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start dot -Tgif output/"+filename+".dot -o output/"+filename+".gif");
+        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start dot -Tjpg output/"+filename+".dot -o output/"+filename+".jpg");
         pb.redirectErrorStream(true);
         Process process = pb.start();
         System.out.println("Exit Code: "+process.waitFor());
