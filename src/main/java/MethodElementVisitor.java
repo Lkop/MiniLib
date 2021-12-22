48a27ff328aebfdc685f4c525a2ad42891b6f88d
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-12-22 02:48:38 +0200

    Adding some changes

--- src/main/java/MethodElementVisitor.java
+++ src/main/java/MethodElementVisitor.java
@@ -2,11 +2,15 @@ import treecomponents.BaseVisitor;
 
 public abstract class MethodElementVisitor<T> extends BaseVisitor<T> {
 
-//    public T visitStartingMethodElement(MethodElement node) {
-//        return super.visitChildren(node);
-//    }
+    public T visitStartingMethodElement(StartingMethodElement node) {
+        return super.visitChildren(node);
+    }
 
     public T visitMethodElement(MethodElement node) {
+        return super.visitChildren(node);
+    }
+
+    public T visitConstructorElement(ConstructorElement node) {
         return super.visitChildren(node);
     }
 
