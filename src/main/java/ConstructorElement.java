1d4b6aae61f7b1a06534f463da88976afcc753e8
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2021-12-22 02:47:06 +0200

    Adding separate ConstructorElement

---
+++ src/main/java/ConstructorElement.java
@@ -0,0 +1,22 @@
+import javassist.CtClass;
+import treecomponents.BaseVisitor;
+
+public class ConstructorElement extends MethodElement{
+
+    public ConstructorElement(String class_longname, String method_name, CtClass[] params, String method_signature, Enum.ExprCall type) {
+        super(class_longname, method_name, params, method_signature, type);
+    }
+
+    public String getGraphvizName(){
+        return getClassLongName() + "_constructor_"+getSerialId();
+    }
+
+    @Override
+    public <T> T accept(BaseVisitor<? extends T> visitor) {
+        MethodElementVisitor v = (MethodElementVisitor)visitor;
+        if (v != null) {
+            return (T) v.visitConstructorElement(this);
+        }
+        return null;
+    }
+}
