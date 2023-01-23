af50497ee452976daa0ed9b853f5272819247c68
Loukas Kopanias <loukaskopanias@gmail.com>
2023-01-23 03:18:13 +0200

    Updating ImageUtils working paths

--- src/main/java/org/lkop/minilib/utils/ImageUtils.java
+++ src/main/java/org/lkop/minilib/utils/ImageUtils.java
@@ -1,5 +1,7 @@
 package org.lkop.minilib.utils;
 
+import org.lkop.minilib.constants.Constants;
+import java.io.File;
 import java.io.IOException;
 
 public final class ImageUtils {
@@ -8,12 +10,12 @@ public final class ImageUtils {
 
     }
 
-    public static void createGIF(String filename) throws IOException, InterruptedException {
-        //ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start dot -Tjpg output/"+filename+".dot -o output/"+filename+".jpg");
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start C:\\Users\\Loukas\\Desktop\\Graphviz\\bin\\dot.exe -Tjpg output/"+filename+".dot -o output/"+filename+".jpg");
+    public static void createGIF(String input_file_path) throws IOException, InterruptedException {
+        File input_file = new File(input_file_path);
+        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start dot -Tjpg " + input_file_path + " -o " + input_file.getParent() + "/" + Constants.OUTPUT_FILENAME + ".jpg");
         pb.redirectErrorStream(true);
         Process process = pb.start();
-        System.out.println("Exit Code: "+process.waitFor());
+        System.out.println("Exit Code: " + process.waitFor());
     }
 
 }
