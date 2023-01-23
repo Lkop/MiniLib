b19dca9ecd8003448c4fe0e34259874ccb8c2ca3
Loukas Kopanias <loukaskopanias@gmail.com>
2023-01-23 03:21:03 +0200

    Updating constants

---
+++ src/main/java/org/lkop/minilib/constants/Constants.java
@@ -0,0 +1,9 @@
+package org.lkop.minilib.constants;
+
+public class Constants {
+    public static final String[] PRIMITIVES = { "byte", "short", "int", "long", "float", "double", "boolean", "char" };
+
+    public static final String[] GENERICS = { "T", "? super T" };
+
+    public static final String OUTPUT_FILENAME = "mini-tree";
+}
