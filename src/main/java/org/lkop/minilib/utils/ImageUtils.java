d778ffe3ac0eeb168f894cd021013d8ff99da719
Panagiotis Sotiropoulos <panossoto@gmail.com>
2024-03-10 12:02:08 +0200

    Update ImageUtils.java

--- src/main/java/org/lkop/minilib/utils/ImageUtils.java
+++ src/main/java/org/lkop/minilib/utils/ImageUtils.java
@@ -12,7 +12,7 @@ public final class ImageUtils {
 
     public static void createGIF(String input_file_path) throws IOException, InterruptedException {
     /*    File input_file = new File(input_file_path);
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start dot -Tjpg " + input_file_path + " -o " + input_file.getParent() + "/" + Constants.OUTPUT_FILENAME + ".jpg");
+        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start dot -Tjpg " + input_file_path + " -o " + input_file.getParent() + "/" + Constants.OUTPUT_FILENAME + ".jpg"); // Needs fixing for linux and mac
         pb.redirectErrorStream(true);
         Process process = pb.start();
         System.out.println("Exit Code: " + process.waitFor());*/
