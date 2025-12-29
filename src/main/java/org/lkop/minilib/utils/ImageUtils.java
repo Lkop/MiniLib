cbd9dd5ac403b42511fca1abb9b98a77d80011b1
Loukas Kopanias <37883287+Lkop@users.noreply.github.com>
2025-12-29 17:00:47 +0100

    Changing tree image generation to svg

--- src/main/java/org/lkop/minilib/utils/ImageUtils.java
+++ src/main/java/org/lkop/minilib/utils/ImageUtils.java
@@ -13,7 +13,7 @@ public final class ImageUtils {
     public static void createGIF(String input_file_path) throws IOException, InterruptedException {
         //TODO check for dot path
         File input_file = new File(input_file_path);
-        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start dot -Tjpg " + input_file_path + " -o " + input_file.getParent() + "/" + Constants.OUTPUT_FILENAME + ".jpg");
+        ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start dot -Tsvg " + input_file_path + " -o " + input_file.getParent() + "/" + Constants.OUTPUT_FILENAME + ".svg");
         //ProcessBuilder pb = new ProcessBuilder("cmd.exe", "/c", "start C:\\Users\\Loukas\\Desktop\\Graphviz\\bin\\dot.exe -Tjpg " + input_file_path + " -o " + input_file.getParent() + "/" + Constants.OUTPUT_FILENAME + ".jpg");
         pb.redirectErrorStream(true);
         Process process = pb.start();
